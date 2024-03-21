df342243681b88bdaa486d45343566c0190818fa
unknown <albertkantwi@gmail.com>
2024-03-21 11:22:31 +0000

    Update API data into project

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const apiKey = "68a77b33afb768bb3bbc647f0a90d2c9";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metrics&q=accra";
+const apiKey = '68a77b33afb768bb3bbc647f0a90d2c9';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=accra';
 
 async function checkWeather(){
     const response = await fetch(apiURL + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
 }
 
 checkWeather();
