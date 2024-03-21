ab3ede0d2b8b63b9d44af5f892ce4233b68272e6
unknown <albertkantwi@gmail.com>
2024-03-21 12:36:04 +0000

    Add functions to display weather information

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 const apiKey = '68a77b33afb768bb3bbc647f0a90d2c9';
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=accra';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
-async function checkWeather(){
-    const response = await fetch(apiURL + `&appid=${apiKey}`);
-    var data = await response.json();
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon');
 
-    console.log(data);
+async function checkWeather(city){
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+    
+    if(response.status == 404){
+        document.querySelector('.error').style.display = 'block';
+        document.querySelector('.weather').style.display = 'none';
+    }
+    else {
+        var data = await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
+    if(data.weather[0].main == 'Clouds'){
+        weatherIcon.src = 'images/clouds.png';
+    }
+    else if(data.weather[0] == 'Clear'){
+        weatherIcon.src = 'images/clear.png';
+    }
+    else if(data.weather[0] == 'Rain'){
+        weatherIcon.src = 'images/rain.png';
+    }
+    else if(data.weather[0] == 'Drizzle'){
+        weatherIcon.src = 'images/drizzle.png';
+    }
+    else if(data.weather[0] == 'Mist'){
+        weatherIcon.src = 'images/mist.png';
+    }
+    
+    document.querySelector('.weather').style.display = 'block';
+    document.querySelector('.error').style.display = 'none';
+
+
+    }
+
 }
 
-checkWeather();
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(searchBox.value);
+})
